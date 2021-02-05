47b8a70cd3fc5995c20044708b1d3b7d6c811ab7
Vadim Lunev <lunev82@gmail.com>
2021-02-05 14:52:01 +0300

    refactoring: close popups by close button & overlay click

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,9 @@ const popupTypeUser = document.querySelector(".popup_type_user");
 const popupTypeCard = document.querySelector(".popup_type_card");
 
 // выбираем элементы кнопки Редактирования профиля
-const popupCloseButton = popupTypeUser.querySelector(".popup__close");
 const editButton = document.querySelector(".profile__edit-button");
 
 // выбираем элементы кнопки Добавления карточки
-const popupCloseButtonCard = popupTypeCard.querySelector(".popup__close");
 const addButton = document.querySelector(".profile__add-button");
 
 // выбираем элементы формы Редактирования профиля
@@ -45,25 +43,24 @@ function fillValue() {
   subTitleField.value = subTitle.textContent;
 }
 
-//Функция закрытия попапа Редактирования профиля по клику на фон
-function popupClickHandlerTypeUser(event) {
-  if (event.target.classList.contains("popup_type_user")) {
-    closePopup(popupTypeUser);
-  }
-}
+//Функция закрытия всех попапов по клику на оверлей или крестик
+const popups = document.querySelectorAll(".popup");
+popups.forEach((popup) => {
+  popup.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("popup_opened")) {
+      closePopup(popup);
+    }
+    if (evt.target.classList.contains("popup__close")) {
+      closePopup(popup);
+    }
+  });
+});
 
 //Функция закрытия всех попапов по нажатию на Escape
 function closeByEscape(evt) {
   if (evt.key === "Escape") {
     const openedPopup = document.querySelector(".popup_opened");
     closePopup(openedPopup);
-  }
-}
-
-// Функция закрытия попапа Добавления карточки по клику на фон
-function popupClickHandlerTypeCard(event) {
-  if (event.target.classList.contains("popup_type_card")) {
-    closePopup(popupTypeCard);
   }
 }
 
@@ -87,33 +84,15 @@ editButton.addEventListener("click", () => {
   openPopup(popupTypeUser);
   fillValue();
 });
-popupCloseButton.addEventListener("click", () => closePopup(popupTypeUser));
-popupTypeUser.addEventListener("mousedown", popupClickHandlerTypeUser);
 form.addEventListener("submit", submitFormTypeUser);
 
 //обработчик событий формы Добавления карточки
 addButton.addEventListener("click", () => openPopup(popupTypeCard));
-popupCloseButtonCard.addEventListener("click", () => closePopup(popupTypeCard));
-popupTypeCard.addEventListener("mousedown", popupClickHandlerTypeCard);
 
 // выбираем элементы popup preview карточки
 const popupTypePreview = document.querySelector(".popup_type_preview");
 const popupImgPreview = popupTypePreview.querySelector(".popup__img-preview");
 const popupImgTitle = popupTypePreview.querySelector(".popup__img-title");
-const ImgPreviewCloseButton = popupTypePreview.querySelector(".popup__close");
-
-//Функция закрытия popup preview карточки по клику на фон
-function popupClickHandlerTypePreview(event) {
-  if (event.target.classList.contains("popup_type_preview")) {
-    closePopup(popupTypePreview);
-  }
-}
-
-//обработчик событий popup preview карточки
-ImgPreviewCloseButton.addEventListener("click", () =>
-  closePopup(popupTypePreview)
-);
-popupTypePreview.addEventListener("mousedown", popupClickHandlerTypePreview);
 
 // загружаем первоначальные карточки
 const initialCards = [
@@ -150,7 +129,6 @@ const initialCards = [
 ];
 
 const cardList = document.querySelector(".cards__list");
-
 initialCards.forEach((dataCard) => {
   const card = createCard(dataCard);
   cardList.append(card);
@@ -183,7 +161,6 @@ function createCard(dataCard) {
     popupImgPreview.src = event.target.src;
     popupImgTitle.textContent = elem.name;
   });
-
   return templateCard;
 }
 
