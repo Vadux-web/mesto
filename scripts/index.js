703d07a4a94e51f0c267d7eb86ef6bcacee381db
Vadim Lunev <lunev82@gmail.com>
2021-04-26 16:22:11 +0300

    refactoring using classes

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+import Cards from "./Cards.js";
+import FormValidator from "./FormValidator.js";
+
 //============РЕДАКТИРОВАНИЕ ПРОФИЛЯ================
 
 // выбираем элементы попапа Редактирования профиля
@@ -23,17 +26,10 @@ function fillValue() {
 
 // функция отправки формы Редактирования профиля
 function submitFormTypeUser(event) {
-  const formElement = event.target.closest("form");
-  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
   event.preventDefault();
-  if (hasInvalidInput(inputList)) {
-    // Запретить submit по нажатию на Enter при ошибках валидации
-    return false;
-  } else {
-    title.textContent = titleField.value;
-    subTitle.textContent = subTitleField.value;
-    closePopup(popupTypeUser);
-  }
+  title.textContent = titleField.value;
+  subTitle.textContent = subTitleField.value;
+  closePopup(popupTypeUser);
 }
 
 // обработчик событий формы Редактирования профиля
@@ -41,7 +37,6 @@ editButton.addEventListener("click", () => {
   openPopup(popupTypeUser);
   fillValue();
 });
-
 form.addEventListener("submit", submitFormTypeUser);
 
 //============ФУНКЦИИ ОТКРЫТИЯ/ЗАКРЫТИЯ ПОПАПОВ================
@@ -60,7 +55,7 @@ function closePopup(popup) {
 // функция закрытия всех попапов по клику на оверлей или крестик
 const popups = document.querySelectorAll(".popup");
 popups.forEach((popup) => {
-  popup.addEventListener("click", (evt) => {
+  popup.addEventListener("mousedown", (evt) => {
     if (evt.target.classList.contains("popup_opened")) {
       closePopup(popup);
     }
@@ -70,7 +65,7 @@ popups.forEach((popup) => {
   });
 });
 
-//Функция закрытия всех попапов по нажатию на Escape
+// функция закрытия всех попапов по нажатию на Escape
 function closeByEscape(evt) {
   if (evt.key === "Escape") {
     const openedPopup = document.querySelector(".popup_opened");
@@ -85,15 +80,6 @@ const addButton = document.querySelector(".profile__add-button");
 
 // выбираем элементы попапа Добавления карточки
 const popupTypeCard = document.querySelector(".popup_type_card");
-
-// // выбираем элементы формы Добавления карточки
-// const formCard = popupTypeCard.querySelector(".popup__form");
-// const titleFieldCard = popupTypeCard.querySelector(
-//   ".popup__input_type_img-name"
-// );
-// const subTitleFieldCard = popupTypeCard.querySelector(
-//   ".popup__input_type_img-link"
-// );
 
 //обработчик событий формы Добавления карточки
 addButton.addEventListener("click", () => {
@@ -134,169 +120,34 @@ const initialCards = [
   },
 ];
 
-//==============РАБОТАЕМ С КЛАССАМИ========================
-
-class AddCardForm {
-  _container;
-  _onSubmit;
-
-  constructor(selector, onSubmit) {
-    this._container = document.querySelector(selector);
-    this._onSubmit = onSubmit;
-  }
-  _submit = (event) => {
-    event.preventDefault();
-    const input = Array.from(this._container.querySelectorAll(".popup__input"));
-
-    this._onSubmit(input[0].value);
-
-    input[0].value = "";
-    input[1].value = "";
-    closePopup(popupTypeCard);
-  };
-
-  init = () => {
-    this._container.addEventListener("submit", this._submit);
-  };
-}
-
-// //обработка отправки формы добавления карточки
-// formCard.addEventListener("submit", (event) => {
-//   const formElement = event.target.closest("form");
-//   const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-//   event.preventDefault();
-//   if (hasInvalidInput(inputList)) {
-//     // Запретить submit по нажатию на Enter при ошибках валидации
-//     return false;
-//   } else {
-//     const link = subTitleFieldCard.value;
-//     const name = titleFieldCard.value;
-//     if (!link || !name) return;
-//     const card = createCard({ link, name });
-//     cardList.prepend(card);
-//     formCard.reset();
-//     closePopup(popupTypeCard);
-//   }
-// });
-
-class Card {
-  _dataCard;
-  _template;
-  _view;
-
-  constructor(dataCard, template) {
-    this._dataCard = dataCard;
-    this._template = template;
-  }
-
-  render = () => {
-    this._view = this._template.cloneNode(true);
-    this._view.querySelector(".card__title").textContent = this._dataCard.name;
-    this._view.querySelector(".card__image").src = this._dataCard.link;
-    this._setEventListeners();
-    return this._view;
-  };
-
-  _setEventListeners() {
-    //делаю переменные видимыми для вложенной функции openPopup(popupTypePreview)
-    const dataCardLink = this._dataCard.link;
-    const dataCardName = this._dataCard.name;
-
-    this._view
-      .querySelector(".card__trash-button")
-      .addEventListener("click", (event) => {
-        event.target.closest(".card-li").remove();
-      });
-
-    this._view
-      .querySelector(".card__like-button")
-      .addEventListener("click", function (event) {
-        event.target.classList.toggle("card__like-button_active");
-      });
-
-    this._view
-      .querySelector(".card__image")
-      .addEventListener("click", function () {
-        openPopup(popupTypePreview);
-        popupImgPreview.src = dataCardLink;
-        popupImgTitle.textContent = dataCardName;
-      });
-
-    // выбираем элементы popup preview карточки
-    const popupTypePreview = document.querySelector(".popup_type_preview");
-    const popupImgPreview = popupTypePreview.querySelector(
-      ".popup__img-preview"
-    );
-    const popupImgTitle = popupTypePreview.querySelector(".popup__img-title");
-
-    return this._template;
-  }
-}
-
-class Cards {
-  _container;
-  _itemTemplate;
-  _form;
-
-  constructor(selector, itemTemplate) {
-    this._container = document.querySelector(selector);
-    this._itemTemplate = itemTemplate;
-
-    this._form = new AddCardForm(".add-card-form", this.addCard);
-    this._form.init();
-  }
-
-  addCard = (dataCard) => {
-    const item = new Card(dataCard, this._itemTemplate);
-    const renderResult = item.render();
-    this._container.append(renderResult);
-  };
-}
-
-//создаем сущность Cards, через создание класса
+// создаем сущность Cards, через создание класса
 const cardsNew = new Cards(
   ".cards__list",
-  document.querySelector(".card-template").content
+  document.querySelector(".card-template").content,
+  () => closePopup(popupTypeCard)
 );
 
-//перебераем массив данных
+// перебераем массив данных
 initialCards.forEach((dataCard) => {
   cardsNew.addCard(dataCard);
 });
 
-//======================================
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__submit",
+  inactiveButtonClass: "popup__submit_inactive",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_active",
+};
 
-// const cardList = document.querySelector(".cards__list");
-// initialCards.forEach((dataCard) => {
-//   const card = createCard(dataCard);
-//   cardList.append(card);
-// });
-//
-// function createCard(dataCard) {
-//   const templateCard = document
-//     .querySelector(".card-template")
-//     .content.cloneNode(true);
-//   const cardLink = templateCard.querySelector(".card__image");
-//   cardLink.src = dataCard.link;
-//   const cardName = templateCard.querySelector(".card__title");
-//   cardName.textContent = dataCard.name;
-//   cardLink.alt = dataCard.name;
+// находим все формы с указанным классом в DOM, сделаем из них массив методом Array.from
+const formList = Array.from(
+  document.querySelectorAll(validationConfig.formSelector)
+);
 
-// //обработка отправки формы добавления карточки
-// formCard.addEventListener("submit", (event) => {
-//   const formElement = event.target.closest("form");
-//   const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-//   event.preventDefault();
-//   if (hasInvalidInput(inputList)) {
-//     // Запретить submit по нажатию на Enter при ошибках валидации
-//     return false;
-//   } else {
-//     const link = subTitleFieldCard.value;
-//     const name = titleFieldCard.value;
-//     if (!link || !name) return;
-//     const card = createCard({ link, name });
-//     cardList.prepend(card);
-//     formCard.reset();
-//     closePopup(popupTypeCard);
-//   }
-// });
+// перебираем полученную коллекцию
+formList.forEach((formElement) => {
+  const formValidator = new FormValidator(validationConfig, formElement);
+  formValidator.enableValidation();
+});
