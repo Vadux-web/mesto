99888668aec0113b43f1e30cf9e790b11fb68530
Vadim Lunev <lunev82@gmail.com>
2021-06-28 16:53:44 +0300

    add Webpack

---
+++ src/scripts/FormValidator.js
@@ -0,0 +1,95 @@
+class FormValidator {
+  constructor(settings, formElement) {
+    this._settings = settings;
+    this._formElement = formElement;
+    this._buttonElement = formElement.querySelector(
+      settings.submitButtonSelector
+    );
+
+    // Находим все поля внутри формы, сделаем из них массив методом Array.from
+    this._inputList = Array.from(
+      formElement.querySelectorAll(settings.inputSelector)
+    );
+    this._buttonElement.classList.add(this._settings.inactiveButtonClass);
+  }
+
+  toggleButtonState = () => {
+    // Если есть хотя бы один невалидный инпут
+    if (this._hasInvalidInput(this._inputList)) {
+      // сделай кнопку неактивной
+      this._buttonElement.classList.add(this._settings.inactiveButtonClass);
+    } else {
+      // иначе сделай кнопку активной
+      this._buttonElement.classList.remove(this._settings.inactiveButtonClass);
+    }
+  };
+
+  _validate = (inputElement) => {
+    if (!inputElement.validity.valid) {
+      // showInputError теперь получает параметром форму, в которой
+      // находится проверяемое поле, и само это поле
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      // hideInputError теперь получает параметром форму, в которой
+      // находится проверяемое поле, и само это поле
+      this._hideInputError(inputElement);
+    }
+  };
+
+  enableValidation() {
+    this._formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault(); // У каждой формы отменим стандартное поведение
+      if (this._hasInvalidInput(this._inputList)) {
+        return false;
+      }
+    });
+
+    // Обойдём все элементы полученной коллекции
+    this._inputList.forEach((inputElement) => {
+      // каждому полю добавим обработчик события input
+      inputElement.addEventListener("input", () => {
+        // Внутри колбэка вызовем Validate, передав ей форму и проверяемый элемент
+        this._validate(inputElement);
+        // Вызовем toggleButtonState и передадим ей массив полей и кнопку
+        this.toggleButtonState();
+      });
+    });
+  }
+
+  _showInputError = (inputElement, errorMessage) => {
+    // Находим элемент ошибки внутри самой функции
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.id}-error`
+    );
+    // добавляем класс ошибки элементу input
+    inputElement.classList.add(this._settings.inputErrorClass); //border-bottom: 1px solid red;
+    // Заменим содержимое span с ошибкой на переданный параметр
+    errorElement.textContent = errorMessage;
+    // Показываем сообщение об ошибке
+    errorElement.classList.add(this._settings.errorClass);
+  };
+
+  // Функция, которая удаляет класс с ошибкой
+  _hideInputError = (inputElement) => {
+    // Находим элемент ошибки
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.id}-error`
+    );
+    inputElement.classList.remove(this._settings.inputErrorClass);
+    // Скрываем сообщение об ошибке
+    errorElement.classList.remove(this._settings.errorClass);
+    // Очистим ошибку
+    errorElement.textContent = "";
+  };
+
+  _hasInvalidInput = (inputList) => {
+    // проходим по этому массиву методом some
+    return inputList.some((inputElement) => {
+      // если поле не валидно, колбэк вернёт true
+      // обход массива прекратится и вся фунцкция вернёт true
+      return !inputElement.validity.valid;
+    });
+  };
+}
+
+export default FormValidator;
---
+++ src/scripts/index.js
@@ -0,0 +1,158 @@
+import Card from "./Card.js";
+import FormValidator from "./FormValidator.js";
+import initialCards from "./initialCards";
+import "../pages/index.css";
+
+//============РЕДАКТИРОВАНИЕ ПРОФИЛЯ================
+
+// выбираем элементы попапа Редактирования профиля
+const popupTypeUser = document.querySelector(".popup_type_user");
+
+// выбираем элементы кнопки Редактирования профиля
+const editButton = document.querySelector(".profile__edit-button");
+
+// выбираем элементы формы Редактирования профиля
+const title = document.querySelector(".profile__title");
+const subTitle = document.querySelector(".profile__subtitle");
+const form = popupTypeUser.querySelector(".popup__form");
+const titleField = popupTypeUser.querySelector(".popup__input_type-title");
+const subTitleField = popupTypeUser.querySelector(
+  ".popup__input_type-subtitle"
+);
+
+// функция заполнения полей Редактирования профиля при открытии
+function fillValue() {
+  titleField.value = title.textContent;
+  subTitleField.value = subTitle.textContent;
+}
+
+//Функция отправки формы Редактирования профиля
+function submitFormTypeUser(event) {
+  const formElement = event.target.closest("form");
+  event.preventDefault();
+  title.textContent = titleField.value;
+  subTitle.textContent = subTitleField.value;
+  closePopup(popupTypeUser);
+}
+
+//обработчик событий формы Редактирования профиля
+editButton.addEventListener("click", () => {
+  openPopup(popupTypeUser);
+  fillValue();
+});
+form.addEventListener("submit", submitFormTypeUser);
+
+//============ФУНКЦИИ ОТКРЫТИЯ/ЗАКРЫТИЯ ПОПАПОВ================
+
+// функция открытия попапов
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+  document.addEventListener("keydown", closeByEscape);
+}
+
+// функция закрытия попапов
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+  document.removeEventListener("keydown", closeByEscape);
+}
+
+//Функция закрытия всех попапов по клику на оверлей или крестик
+const popups = document.querySelectorAll(".popup");
+popups.forEach((popup) => {
+  popup.addEventListener("mousedown", (evt) => {
+    if (evt.target.classList.contains("popup_opened")) {
+      closePopup(popup);
+    }
+    if (evt.target.classList.contains("popup__close")) {
+      closePopup(popup);
+    }
+  });
+});
+
+//Функция закрытия всех попапов по нажатию на Escape
+function closeByEscape(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_opened");
+    closePopup(openedPopup);
+  }
+}
+
+//============ДОБАВЛЕНИЕ КАРТОЧЕК================
+
+// выбираем элементы кнопки Добавления карточки
+const addButton = document.querySelector(".profile__add-button");
+
+// выбираем элементы попапа Добавления карточки
+const popupTypeCard = document.querySelector(".popup_type_card");
+
+// выбираем элементы формы Добавления карточки
+const formCard = popupTypeCard.querySelector(".popup__form");
+const titleFieldCard = popupTypeCard.querySelector(
+  ".popup__input_type_img-name"
+);
+const subTitleFieldCard = popupTypeCard.querySelector(
+  ".popup__input_type_img-link"
+);
+
+//обработчик событий формы Добавления карточки
+addButton.addEventListener("click", () => openPopup(popupTypeCard));
+
+const cardList = document.querySelector(".cards__list");
+initialCards.forEach((dataCard) => {
+  const template = document.querySelector(".card-template").content;
+  const card = new Card(dataCard, template, handleOpenPopup);
+  const renderResult = card.render();
+  cardList.append(renderResult);
+});
+
+// функция открытия попапа Preview карточки
+function handleOpenPopup(name, link) {
+  //принимает данные
+  const popupTypePreview = document.querySelector(".popup_type_preview");
+  const popupImgPreview = popupTypePreview.querySelector(".popup__img-preview");
+  const popupImgTitle = popupTypePreview.querySelector(".popup__img-title");
+  openPopup(popupTypePreview);
+  popupImgPreview.src = name;
+  popupImgTitle.textContent = link;
+}
+
+//обработка отправки формы добавления карточки
+formCard.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const template = document.querySelector(".card-template").content;
+  const link = subTitleFieldCard.value;
+  const name = titleFieldCard.value;
+  if (!link || !name) return;
+  const card = new Card({ link, name }, template);
+  const renderResult = card.render();
+  cardList.prepend(renderResult);
+  formCard.reset();
+
+  closePopup(popupTypeCard);
+});
+
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__submit",
+  inactiveButtonClass: "popup__submit_inactive",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_active",
+};
+
+// находим все формы с указанным классом в DOM, сделаем из них массив методом Array.from
+const formList = Array.from(
+  document.querySelectorAll(validationConfig.formSelector)
+);
+
+// перебираем полученную коллекцию
+formList.forEach((formElement) => {
+  const formValidator = new FormValidator(validationConfig, formElement);
+  formValidator.enableValidation();
+  formValidator.toggleButtonState();
+  formElement.addEventListener("submit", (evt) => {
+    formValidator.toggleButtonState();
+  });
+});
+
+export {};
